901001f4fc424fe0ad0ff60f9f338891b85165b4
Filipe Branco <estt6967@ipt.pt>
2026-03-09 12:53:33 +0000

    configueações  de model da fotografia e category

--- API/API/Models/Category.cs
+++ API/API/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     /// <summary>
     /// categoria que a foto tem
@@ -12,7 +14,10 @@
         /// <summary>
         /// nome da categoria
         /// </summary>
-        public string Name { get; set; }
+        [StringLength(20)] // dimensão máxima do texto a introduzir
+        [Required(ErrorMessage ="O nome da categoria é obrigatório")] // mensagem de erro quando não é inserido nome da categoria
+        [Display(Name="Nome da Categoria")] // o nome que irá aparecer no ecrã
+        public string Name { get; set; } = "";
 
         /* ********************************************
          * Relacionamentos
@@ -21,6 +26,6 @@
         /// <summary>
         /// Lista de fotografias que a categoria tem
         /// </summary>
-        public ICollection<Photography> ListOfPhotos { get; set; }
+        public ICollection<Photography> ListOfPhotos { get; set; } = [];
     }
 }
--- API/API/Models/Photography.cs
+++ API/API/Models/Photography.cs
@@ -16,22 +16,32 @@ namespace API.Models
         /// <summary>
         /// nome da foto
         /// </summary>
-        public string Title { get; set; }
+        [StringLength(50)]
+        [Required(ErrorMessage ="O {0} é obrigatório")]
+        [Display(Name ="Título")]
+        public string Title { get; set; }=string.Empty; // <=> "";
         /// <summary>
         /// descrição da foto
         /// </summary>
-        public string Description { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Descrição")]
+        public string? Description { get; set; }
         /// <summary>
         /// nome do ficheiro da fotografia
         /// </summary>
-        public string File { get; set; }
+        [StringLength(40)]
+        public string File { get; set; } = "";
         /// <summary>
         /// data em que foi colocada a fotografia
         /// </summary>
+        [Display(Name = "Data")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         /// <summary>
         /// preço da fotografia
         /// </summary>
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
 
 
@@ -47,7 +57,7 @@ namespace API.Models
         /// <summary>
         /// FK para Category da foto
         /// </summary>
-        public Category Category { get; set; }
+        public Category Category { get; set; } = null!;
 
         /* ********************************************
          * Relacionamentos N-M
@@ -56,6 +66,6 @@ namespace API.Models
         /// <summary>
         /// Lista de compras que o uma foto foi comprada
         /// </summary>
-        public ICollection<Purchase> ListOfPurchase { get; set; }
+        public ICollection<Purchase> ListOfPurchase { get; set; } = [];
     }
 }
