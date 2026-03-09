e53d37ee2c2e78a3ec9c4c08ce65eecaf3930a6c
Filipe Branco <estt6967@ipt.pt>
2026-03-09 10:01:00 +0000

    Classes para o model

---
+++ API/API/Models/Photography.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class Photography
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string File { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ API/API/Models/Purchase.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public State State { get; set; }
+    }
+    public enum State
+    {
+        Pending,
+        Paid,
+        Sent,
+        Delivery,
+        Closed
+    }
+}
