acbe4ac6a31324ead5afe8e508b08652bd764ae8
Filipe Branco <estt6967@ipt.pt>
2026-03-09 10:55:13 +0000

    criação de tabelas da base de dados

--- API/API/Data/ApplicationDbContext.cs
+++ API/API/Data/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    /// <summary>
+    /// Base de dados do nosso projeto
+    /// </summary>
+    /// <param name="options"></param>
+    public class ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : IdentityDbContext(options)
     {
+
+        /* tabelas da base de dados */
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Photography> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<MyUser> Users { get; set; }
+
     }
 }
